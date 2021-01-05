1708c3df6e1053bfdef6184af7a972285de203d0
weru <fromweru@gmail.com>
2021-01-05 14:26:13 +0300

    save enum match

---
+++ enum_match/src/main.rs
@@ -0,0 +1,54 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState)
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin:: Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    value_in_cents(Coin::Penny);
+    value_in_cents(Coin::Quarter(UsState::Alaska));
+
+    let five = Some(5);
+    let _six = plus_one(five);
+    let _none = plus_one(None);
+
+    // println!("six {}, none {}", six, none);
+
+    let some_u8_value = 0u8;
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => ()
+    }
+}
